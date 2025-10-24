07b89523da5e6ef7bcf53174a851844a64b462f7
kal-i <paul.maceres00@gmail.com>
2025-10-24 19:34:40 +0800

    feat(data): Add JSON formatted data experience

---
+++ js/data/experience.js
@@ -0,0 +1,11 @@
+export const experience = [
+    {
+        role: "IT Intern",
+        host: "DepEd Legazpi City Division",
+        duration: "Feb - Jun 2025",
+        description: [
+            "Assisted in encoding, organizing, and documenting supply inventory for the Supply Section.",
+            "Led the development of a capstone inventory system, applying real workflow insights, client feedback, and iterative testing to enhance features and document generation, and deployed it by seting up a local server and building a Windows installer."
+        ],
+    }
+];

afa8ae719161799905c599287d5693e03e29b5a8
kal-i <paul.maceres00@gmail.com>
2025-10-24 19:45:23 +0800

    chore: Remove setupModal and add setupProjectsRedirect

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
+import { renderExperience } from "./js/components/experienceRenderer.js";
 import { renderSkills } from "./js/components/skillRenderer.js";
 import { renderProjects } from "./js/components/projectRenderer.js";
-import { setupModal } from "./js/components/modal.js";
+import { setupProjectsRedirect } from "./js/components/setupProjectsRedirect.js";
 import { setupScrollToRevealAnimation } from "./js/components/reveal.js";
 
 document.addEventListener("DOMContentLoaded", () => {
+  renderExperience();
   renderSkills();
   renderProjects();
-  setupModal();
+  setupProjectsRedirect();
   setupScrollToRevealAnimation();
 });
